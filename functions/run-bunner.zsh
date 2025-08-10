4bce2a6ad72115fe1fdb1f9e44ad606f07bda8c8
Petr Fusek <petr.fusek97@gmail.com>
2025-08-10 17:22:34 +0200

    refactor: replace run-bunner function calls with direct brun command in scripts

--- functions/run-bunner.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-source "$SCRIPT_FNS/log.zsh"
-
-function run-bunner() {
-    local command_directory=$(realpath "$SCRIPTS/../bunner-commands")
-    local bunner_run_script=$(realpath "$SCRIPTS/../bunner/run")
-    $bunner_run_script "$command_directory" "$@"
-}
