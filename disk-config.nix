2f5494faac4dd25f30e7e5c854f86aff91347211
Petr Fusek <petr.fusek97@gmail.com>
2025-10-31 14:38:12 +0100

    Update disk device path from '/dev/vda' to '/dev/sda' in disk configuration

--- disk-config.nix
+++ disk-config.nix
@@ -3,7 +3,7 @@
     disk = {
       main = {
         type = "disk";
-        device = "/dev/vda";
+        device = "/dev/sda";
         content = {
           type = "gpt";
           partitions = {
