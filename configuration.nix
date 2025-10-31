4da1723ee6ef4ab6075473f41f3b236a803c71ac
Petr Fusek <petr.fusek97@gmail.com>
2025-10-31 17:19:45 +0100

    Add XDG Desktop Portal configuration for screen casting and file pickers

--- configuration.nix
+++ configuration.nix
@@ -30,6 +30,13 @@
   
   programs.firefox.enable = true;
   
+  # XDG Desktop Portal for screen casting, file pickers, etc.
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    config.common.default = "gtk";
+  };
+  
   environment.systemPackages = with pkgs; [
     accountsservice    # For Dank Material Shell user menu
     alacritty          # Terminal emulator
