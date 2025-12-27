0b2f1ddd2437d0656effbd6309c3d7085dac5e92
Petr Fusek <petr.fusek97@gmail.com>
2025-12-27 16:29:28 +0100

    Comment out unused dankMaterialShell program configuration in home.nix

--- home.nix
+++ home.nix
@@ -3,7 +3,7 @@
 {
   imports = [
     inputs.niri.homeModules.niri
-    inputs.dankMaterialShell.homeModules.dankMaterialShell.default
+    # inputs.dankMaterialShell.homeModules.dankMaterialShell.default
     # Don't import the niri module - we're using our own niri config
   ];
 
@@ -11,20 +11,20 @@
   home.homeDirectory = "/home/petr";
   home.stateVersion = "25.05";
 
-  programs.dankMaterialShell = {
-    enable = true;
-    # Core features
-    enableSystemd = true;           # Systemd service for auto-start
-    enableSystemMonitoring = true;  # System monitoring widgets (dgop)
-    enableClipboard = true;         # Clipboard history manager
-    enableVPN = true;               # VPN management widget
-    enableBrightnessControl = true; # Backlight/brightness controls
-    enableColorPicker = true;       # Color picker tool
-    enableDynamicTheming = true;    # Wallpaper-based theming (matugen)
-    enableAudioWavelength = true;   # Audio visualizer (cava)
-    enableCalendarEvents = true;    # Calendar integration (khal)
-    enableSystemSound = true;       # System sound effects
-  };
+  # programs.dankMaterialShell = {
+  #   enable = true;
+  #   # Core features
+  #   enableSystemd = true;           # Systemd service for auto-start
+  #   enableSystemMonitoring = true;  # System monitoring widgets (dgop)
+  #   enableClipboard = true;         # Clipboard history manager
+  #   enableVPN = true;               # VPN management widget
+  #   enableBrightnessControl = true; # Backlight/brightness controls
+  #   enableColorPicker = true;       # Color picker tool
+  #   enableDynamicTheming = true;    # Wallpaper-based theming (matugen)
+  #   enableAudioWavelength = true;   # Audio visualizer (cava)
+  #   enableCalendarEvents = true;    # Calendar integration (khal)
+  #   enableSystemSound = true;       # System sound effects
+  # };
 
   programs.git = {
     enable = true;
