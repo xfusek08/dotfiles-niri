24b2790ed8a475ffbc5dfda48e4816e7afe92e40
Petr Fusek <petr.fusek97@gmail.com>
2025-12-27 15:22:21 +0100

    Update

--- flake.nix
+++ flake.nix
@@ -58,7 +58,7 @@
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
-        home-manager.nixosModules.home-manager
+        # home-manager.nixosModules.home-manager
       ];
     };
   };
