1d66b820828fb2e15f8bcacc6f803ffb2acfcd76
Petr Fusek <petr.fusek97@gmail.com>
2026-01-16 11:33:12 +0100

    feat: remove pref-by-location plugin and add folder-rules plugin with custom sorting logic

---
+++ configuration/yazi/.config/yazi/plugins/folder-rules.yazi/main.lua
@@ -0,0 +1,15 @@
+-- https://yazi-rs.github.io/docs/tips/#folder-rules
+
+local function setup()
+    ps.sub("ind-sort", function(opt)
+        local cwd = cx.active.current.cwd
+        if cwd:ends_with("Stažené") then
+            opt.by, opt.reverse, opt.dir_first = "mtime", true, true
+        else
+            opt.by, opt.reverse, opt.dir_first = "natural", false, true
+        end
+        return opt
+    end)
+end
+
+return { setup = setup }
