b5274cea4b61d8efdd4e4b185f0760e6ad6407fd
Petr Fusek <petr.fusek97@gmail.com>
2026-05-09 21:04:08 +0200

    Update 3 files

--- functions/log.zsh
+++ functions/log.zsh
@@ -1,14 +1,16 @@
 #!/usr/bin/env zsh
 
 # Simple logging utility with colored output
-# Usage: log [-i|-w|-e|-s] "message"
+# Usage: log [-i|-w|-e|-s] [-f <file>] "message"
 # Options:
 #   -i: info (default, blue)
 #   -w: warning (yellow)
 #   -e: error (red)
 #   -s: success (green)
+#   -f <file>: append plain (no color) output to file instead of stderr
 
 function log() {
+    local OPTIND=1
     local color_reset="\033[0m"
     local color_blue="\033[0;34m"
     local color_red="\033[0;31m"
@@ -17,9 +19,10 @@ function log() {
 
     local type="INFO"
     local color="$color_blue"
+    local output_file=""
 
     # Process options
-    while getopts "iwes" opt; do
+    while getopts "iwesf:" opt; do
         case "$opt" in
             i)
                 type="INFO"
@@ -37,11 +40,19 @@ function log() {
                 type="SUCCESS"
                 color="$color_green"
                 ;;
+            f)
+                output_file="$OPTARG"
+                ;;
         esac
     done
 
     shift $((OPTIND - 1))
     local message="$1"
     local timestamp=$(date +"%d. %m. %Y %H:%M:%S")
-    echo -e "[$timestamp] ${color}[$type] ${message}${color_reset}" >&2
+
+    if [[ -n "$output_file" ]]; then
+        echo "[$timestamp] [$type] ${message}" >> "$output_file"
+    else
+        echo -e "[$timestamp] ${color}[$type] ${message}${color_reset}" >&2
+    fi
 }
