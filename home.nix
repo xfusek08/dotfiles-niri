f69ae1b298e657b0eea7c3028fd26765ab05d242
Petr Fusek <petr.fusek97@gmail.com>
2026-06-08 21:05:32 +0200

    feat: specify image MIME types for Loupe as default viewer

--- home.nix
+++ home.nix
@@ -65,7 +65,15 @@ in {
       "text/html"                = "zen.desktop";
       "x-scheme-handler/http"    = "zen.desktop";
       "x-scheme-handler/https"   = "zen.desktop";
-      "image/*"                  = "org.gnome.Loupe.desktop";
+      "image/png"                = "org.gnome.Loupe.desktop";
+      "image/jpeg"               = "org.gnome.Loupe.desktop";
+      "image/webp"               = "org.gnome.Loupe.desktop";
+      "image/gif"                = "org.gnome.Loupe.desktop";
+      "image/bmp"                = "org.gnome.Loupe.desktop";
+      "image/svg+xml"            = "org.gnome.Loupe.desktop";
+      "image/tiff"               = "org.gnome.Loupe.desktop";
+      "image/avif"               = "org.gnome.Loupe.desktop";
+      "image/heic"               = "org.gnome.Loupe.desktop";
     };
   };
 
